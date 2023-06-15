198277f91cd3be155885486d1679b039d3e04479
YuTanaka <tanakaedu@users.noreply.github.com>
2023-06-15 15:11:22 +0900

    ラベル３つ移動

--- v0615/Form1.cs
+++ v0615/Form1.cs
@@ -2,9 +2,29 @@ namespace v0615
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
+        int vx1 = rand.Next(-10, 11);
+        int vy1 = rand.Next(-10, 11);
+        int vx2 = rand.Next(-10, 11);
+        int vy2 = rand.Next(-10, 11);
+        int vx3 = rand.Next(-10, 11);
+        int vy3 = rand.Next(-10, 11);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx1;
+            label1.Top += vy1;
+
+            label2.Left += vx2;
+            label2.Top += vy2;
+
+            label3.Left += vx3;
+            label3.Top += vy3;
         }
     }
 }
