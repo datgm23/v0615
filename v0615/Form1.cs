24b2af08ecf419374ffef9f6cddad0fea0593c1b
YuTanaka <tanakaedu@users.noreply.github.com>
2023-06-22 15:46:26 +0900

    演習15-2完了

--- v0615/Form1.cs
+++ v0615/Form1.cs
@@ -2,16 +2,17 @@ namespace v0615
 {
     public partial class Form1 : Form
     {
+        int ChrMax = 100;
         static Random rand = new Random();
-        int[] vx = new int[3];
-        int[] vy = new int[3];
-        Label[] labels = new Label[100];
+        int[] vx = new int[1000];
+        int[] vy = new int[1000];
+        Label[] labels = new Label[1000];
 
         public Form1()
         {
             InitializeComponent();
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < ChrMax; i++)
             {
                 vx[i] = rand.Next(-10, 11);
                 vy[i] = rand.Next(-10, 11);
@@ -25,61 +26,26 @@ namespace v0615
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += vx[0];
-            label1.Top += vy[0];
-            if (label1.Left < 0)
+            for (int i = 0; i < ChrMax; i++)
             {
-                vx[0] = Math.Abs(vx[0]);
-            }
-            else if (label1.Right > ClientSize.Width)
-            {
-                vx[0] = -Math.Abs(vx[0]);
-            }
-            if (label1.Top < 0)
-            {
-                vy[0] = Math.Abs(vy[0]);
-            }
-            else if (label1.Bottom > ClientSize.Height)
-            {
-                vy[0] = -Math.Abs(vy[0]);
-            }
-
-            label2.Left += vx[1];
-            label2.Top += vy[1];
-            if (label2.Left < 0)
-            {
-                vx[1] = Math.Abs(vx[1]);
-            }
-            else if (label2.Right > ClientSize.Width)
-            {
-                vx[1] = -Math.Abs(vx[1]);
-            }
-            if (label2.Top < 0)
-            {
-                vy[1] = Math.Abs(vy[1]);
-            }
-            else if (label2.Bottom > ClientSize.Height)
-            {
-                vy[1] = -Math.Abs(vy[1]);
-            }
-
-            label3.Left += vx[2];
-            label3.Top += vy[2];
-            if (label3.Left < 0)
-            {
-                vx[2] = Math.Abs(vx[2]);
-            }
-            else if (label3.Right > ClientSize.Width)
-            {
-                vx[2] = -Math.Abs(vx[2]);
-            }
-            if (label3.Top < 0)
-            {
-                vy[2] = Math.Abs(vy[2]);
-            }
-            else if (label3.Bottom > ClientSize.Height)
-            {
-                vy[2] = -Math.Abs(vy[2]);
+                labels[i].Left += vx[i];
+                labels[i].Top += vy[i];
+                if (labels[i].Left < 0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                else if (labels[i].Right > ClientSize.Width)
+                {
+                    vx[i] = -Math.Abs(vx[i]);
+                }
+                if (labels[i].Top < 0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                else if (labels[i].Bottom > ClientSize.Height)
+                {
+                    vy[i] = -Math.Abs(vy[i]);
+                }
             }
         }
 
